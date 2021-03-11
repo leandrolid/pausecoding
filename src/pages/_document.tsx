74e37377f064f0807865f45ae5d21e68f231b93f
leandrolid <61424755+leandrolid@users.noreply.github.com>
2021-03-10 21:45:37 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,11 @@ export default class MyDocument extends Document {
             <Html>
                 <Head>
                     <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+                    <meta name="description" content="O Pausecoding é um App que utiliza a técnica de pomodoro, com o objetivo de melhorar sua produtividade e foco. Desenvolvido durante a Next Level Week #4 da Rocketseat." />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:title" content="pausecoding" />
+                    <meta property="og:description" content="O Move.it é um App que utiliza a técnica de pomodoro, com o objetivo de melhorar sua produtividade e foco. Desenvolvido durante a Next Level Week #4 da Rocketseat." />
+                    <meta property="og:site_name" content="pausecoding" />
                     <link rel="icon" href="logo-full.svg" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
